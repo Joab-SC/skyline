2179caf9ab5857a0fb6f3bf296e38621c8b34f7a
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-12 19:10:01 -0500

    feat: Add domain rule exception

--- src/main/java/com/uniquindio/skyline/domain/exception/ReglaDominioException.java
+++
@@ -1,7 +0,0 @@
-package com.uniquindio.skyline.domain.exception;
-
-public class ReglaDominioException extends RuntimeException {
-  public ReglaDominioException(String message) {
-    super(message);
-  }
-}
