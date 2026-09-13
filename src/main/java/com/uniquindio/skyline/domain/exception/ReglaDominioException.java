91687644cd21bc3bc338d41a750a05858d70d126
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-12 19:08:18 -0500

    feat: Add status for seats

---
+++ src/main/java/com/uniquindio/skyline/domain/exception/ReglaDominioException.java
@@ -0,0 +1,7 @@
+package com.uniquindio.skyline.domain.exception;
+
+public class ReglaDominioException extends RuntimeException {
+  public ReglaDominioException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/uniquindio/skyline/domain/valueObject/SeatStatus.java
@@ -0,0 +1,7 @@
+package com.uniquindio.skyline.domain.valueObject;
+
+public enum SeatStatus {
+
+    AVALIABLE,
+    OCUPPIED,
+}
