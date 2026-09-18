e466d2f2b7fb02215c97d9f018e8f4c3204723d7
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-17 22:34:01 -0500

    feat: add Aircraft

---
+++ src/main/java/com/uniquindio/skyline/domain/entity/Aircraft.java
@@ -0,0 +1,21 @@
+package com.uniquindio.skyline.domain.entity;
+
+import com.uniquindio.skyline.domain.valueObject.AircraftModel;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Aircraft {
+    private AircraftModel aircraftModel;
+    private int numSeats;
+    private List<Seat> seats;
+
+    private Aircraft(AircraftModel aircraftModel, int numSeats, List<Seat> seats) {
+        this.aircraftModel = aircraftModel;
+        this.numSeats = numSeats;
+        this.seats = seats;
+    }
+
+
+}
