f395c9073e3ac1f839027a86f2fd8ee6b856bac5
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-17 22:50:00 -0500

    feat: add Passenger entity

---
+++ src/main/java/com/uniquindio/skyline/domain/entity/Passenger.java
@@ -0,0 +1,9 @@
+package com.uniquindio.skyline.domain.entity;
+
+
+public class Passenger extends Person {
+    private Seat seat;
+    private boolean holder;
+    private String email;
+    private String phoneNumber;
+}
