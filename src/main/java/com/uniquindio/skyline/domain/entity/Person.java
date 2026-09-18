e7d6a995d2c0916934a77c0df69987c0f5d41a2a
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-17 22:45:54 -0500

    feat: add Person entity

---
+++ src/main/java/com/uniquindio/skyline/domain/entity/Person.java
@@ -0,0 +1,13 @@
+package com.uniquindio.skyline.domain.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Person{
+
+    private String iddocument;
+    private String firstName;
+    private LocalDateTime arrivalTime;
+    private LocalDate birthDate;
+    private Gender gender;
+}
