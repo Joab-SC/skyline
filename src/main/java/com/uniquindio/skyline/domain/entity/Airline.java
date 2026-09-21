159b508971130441d32e916a894f8c7c560d40f1
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-09-21 18:55:40 -0500

    feat: add method to get an Aircraft from an Airline

--- src/main/java/com/uniquindio/skyline/domain/entity/Airline.java
+++ src/main/java/com/uniquindio/skyline/domain/entity/Airline.java
@@ -3,15 +3,17 @@ package com.uniquindio.skyline.domain.entity;
 import com.uniquindio.skyline.domain.exception.DomainRuleException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 public class Airline {
     private String name;
-    private List<Aircraft> aircrafts;
+    private HashMap<String, Aircraft> aircrafts;
     private List<String> legIds;
 
 
-    private Airline(String name, List<Aircraft> aircrafts) {
+    private Airline(String name, HashMap<String, Aircraft> aircrafts) {
         this.name = name;
         this.aircrafts = aircrafts;
         this.legIds = new ArrayList<>()   ;
@@ -27,4 +29,9 @@ public class Airline {
         }
         this.legIds.add(legId);
     }
+
+    public Optional<Aircraft> getAircraft(String aircraftId) {
+        return Optional.ofNullable(aircrafts.get(aircraftId));
+    }
+
 }
