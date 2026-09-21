c24e0ce4e15a84850c147245496df2c2ae23ff96
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-09-20 21:57:37 -0500

    feat: add method to create Luggage

--- src/main/java/com/uniquindio/skyline/domain/entity/Luggage.java
+++ src/main/java/com/uniquindio/skyline/domain/entity/Luggage.java
@@ -1,8 +1,21 @@
 package com.uniquindio.skyline.domain.entity;
 
+import com.uniquindio.skyline.domain.exception.DomainRuleException;
+
 public class Luggage {
     private final int maxWeight = 23;
     double price;
 
+    private Luggage(double price) {
+        this.price = price;
+    }
+
+
+    public static Luggage createLuggage(Double luggagePrice) {
+        if(luggagePrice <  0 ){
+            throw new DomainRuleException("The price of the luggage must be pos");
+        }
+        return new Luggage(luggagePrice);
+    }
 
 }
