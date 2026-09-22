64e5760630adad41ec73780a0e52d01a69d6367c
Joab-SC <joableandrosc@gmail.com>
2026-09-22 12:58:07 -0500

    test: add tests for LegSeat creation

---
+++ src/test/java/com/uniquindio/skyline/domain/entity/LegSeatTest.java
@@ -0,0 +1,26 @@
+package com.uniquindio.skyline.domain.entity;
+
+import com.uniquindio.skyline.domain.valueObject.SeatStatus;
+import org.junit.jupiter.api.Test;
+
+import java.security.Security;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LegSeatTest {
+
+    @Test
+    public void LegSeatIsCreatedAvailable() {
+        LegSeat legSeat = LegSeat.createLegSeat("A32");
+        assertEquals(SeatStatus.AVALIABLE, legSeat.getSeatStatus());
+    }
+
+    @Test
+    public void LegSeatIsCreatedWthEmptyPassengerId() {
+        LegSeat legSeat = LegSeat.createLegSeat("B28");
+        assertEquals(Optional.empty(), legSeat.getPassengerId());
+    }
+
+
+}
