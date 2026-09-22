40c7cafe00a54c39c54b562a2d3bb9d1fc34db33
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-22 12:58:16 -0500

    test: add tests for Luggage creation

---
+++ src/test/java/com/uniquindio/skyline/domain/entity/LuggageTest.java
@@ -0,0 +1,30 @@
+package com.uniquindio.skyline.domain.entity;
+
+import com.uniquindio.skyline.domain.exception.DomainRuleException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LuggageTest {
+
+    @Test
+    public void luggageThrowsExceptionWithNegativePrice(){
+        assertThrows(DomainRuleException.class, () -> {
+            Luggage.createLuggage("001",-2000.0);
+
+        });
+    }
+
+    @Test
+    public void luggageIsCreatedWithMaxWeight23(){
+        Luggage luggage = Luggage.createLuggage("002", 50000.0);
+        assertEquals(23,luggage.getMaxWeight());
+    }
+
+    @Test
+    public void twoLuggageWithTheSameIdAreEqual(){
+        Luggage luggageOriginal = Luggage.createLuggage("001", 50000.0);
+        Luggage luggageDuplicated = Luggage.createLuggage("001", 80000.0);
+        assertEquals(luggageOriginal,luggageDuplicated);
+    }
+}
