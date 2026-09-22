38158affdea586a533b214959e5e91ad89778f67
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-09-21 19:07:09 -0500

    feat: add use case for creating leg

---
+++ src/main/java/com/uniquindio/skyline/application/usecase/CreateLegUseCase.java
@@ -0,0 +1,33 @@
+package com.uniquindio.skyline.application.usecase;
+
+import com.uniquindio.skyline.domain.entity.Aircraft;
+import com.uniquindio.skyline.domain.entity.Airline;
+import com.uniquindio.skyline.domain.entity.ExtraService;
+import com.uniquindio.skyline.domain.entity.Leg;
+import com.uniquindio.skyline.domain.exception.DomainRuleException;
+import com.uniquindio.skyline.domain.repository.AirlineRepository;
+import com.uniquindio.skyline.domain.repository.LegRepository;
+import com.uniquindio.skyline.domain.valueObject.Airport;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CreateLegUseCase {
+    private final LegRepository legRepository;
+    private final AirlineRepository airlineRepository;
+
+    public CreateLegUseCase(LegRepository legRepository, AirlineRepository airlineRepository) {
+        this.legRepository = legRepository;
+        this.airlineRepository = airlineRepository;
+    }
+
+    public Leg execute(String id, Airport originAirport, Airport destinationAirport, LocalDateTime departureTime,
+                        LocalDateTime arrivalTime, String airlineId, String aircraftId, double luggagePrice, double price) {
+
+        Airline airline = airlineRepository.findById(airlineId).orElseThrow(() -> new DomainRuleException("Airline not found to create the leg"));
+        Aircraft aircraft = airline.getAircraft(aircraftId).orElseThrow(() -> new DomainRuleException("Aircraft not found to create the leg"));
+        Leg leg = Leg.createLeg(id, originAirport, destinationAirport, departureTime, arrivalTime, aircraft, luggagePrice, price);
+        legRepository.save(leg);
+        return leg;
+    }
+}
