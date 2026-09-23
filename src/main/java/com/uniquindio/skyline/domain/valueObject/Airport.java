1c62b0853bc4b94fba6c0f6096627422bf9cf7e4
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-23 10:23:54 -0500

    feat: add validations for name, acronym and city of Airport

--- src/main/java/com/uniquindio/skyline/domain/valueObject/Airport.java
+++ src/main/java/com/uniquindio/skyline/domain/valueObject/Airport.java
@@ -5,8 +5,14 @@ import com.uniquindio.skyline.domain.exception.DomainRuleException;
 public record Airport(String name, String acronym, City city) {
 
     public Airport{
-        if (name.isBlank() || acronym.isBlank()){
+        if (name == null || name.isBlank()){
             throw new DomainRuleException("The name or acronym can not be blank");
+        }
+        if (acronym == null || acronym.isBlank()){
+            throw new DomainRuleException("The acronym can not be blank");
+        }
+        if (city == null){
+            throw new DomainRuleException("The city can not be blank");
         }
     }
 }
