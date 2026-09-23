c694b4028d6b04ffe89ca465e85f4af7a90145c1
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2026-09-23 10:25:18 -0500

    refactor: replace isEmpty with isBlank

--- src/main/java/com/uniquindio/skyline/domain/valueObject/Seat.java
+++ src/main/java/com/uniquindio/skyline/domain/valueObject/Seat.java
@@ -6,7 +6,7 @@ import com.uniquindio.skyline.domain.exception.DomainRuleException;
 public record Seat (String code) {
 
     public Seat{
-        if (code == null || code.isEmpty()) {
+        if (code == null || code.isBlank()) {
             throw new DomainRuleException("Seat code is required");
         }
     }
