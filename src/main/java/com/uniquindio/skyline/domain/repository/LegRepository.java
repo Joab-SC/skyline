53a094da8f95b9699a24153d43a14e987b8f455d
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-09-23 23:24:04 -0500

    feat: add method in repository to get all the legs with a certain aircraft

--- src/main/java/com/uniquindio/skyline/domain/repository/LegRepository.java
+++ src/main/java/com/uniquindio/skyline/domain/repository/LegRepository.java
@@ -2,9 +2,11 @@ package com.uniquindio.skyline.domain.repository;
 
 import com.uniquindio.skyline.domain.entity.Leg;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LegRepository {
     Optional<Leg> findById(String id);
     void save(Leg leg);
+    List<Leg> findLegsByAircraft(String idAircraft);
 }
