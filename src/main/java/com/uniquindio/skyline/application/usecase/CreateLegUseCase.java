a946e01a8ea28cdbc91fe49ef49bcba3eaf2f377
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-09-23 23:30:34 -0500

    feat: validate overlaping when creating leg in the usecase

--- src/main/java/com/uniquindio/skyline/application/usecase/CreateLegUseCase.java
+++ src/main/java/com/uniquindio/skyline/application/usecase/CreateLegUseCase.java
@@ -7,6 +7,7 @@ import com.uniquindio.skyline.domain.entity.Leg;
 import com.uniquindio.skyline.domain.exception.DomainRuleException;
 import com.uniquindio.skyline.domain.repository.AirlineRepository;
 import com.uniquindio.skyline.domain.repository.LegRepository;
+import com.uniquindio.skyline.domain.service.LegOverlapValidator;
 import com.uniquindio.skyline.domain.valueObject.Airport;
 
 import java.time.LocalDateTime;
@@ -15,10 +16,12 @@ import java.util.List;
 public class CreateLegUseCase {
     private final LegRepository legRepository;
     private final AirlineRepository airlineRepository;
+    private final LegOverlapValidator legOverlapValidator;
 
-    public CreateLegUseCase(LegRepository legRepository, AirlineRepository airlineRepository) {
+    public CreateLegUseCase(LegRepository legRepository, AirlineRepository airlineRepository, LegOverlapValidator legOverlapValidator) {
         this.legRepository = legRepository;
         this.airlineRepository = airlineRepository;
+        this.legOverlapValidator = legOverlapValidator;
     }
 
     // Creates a leg and links it to the airline.
@@ -27,6 +30,7 @@ public class CreateLegUseCase {
 
         Airline airline = airlineRepository.findById(airlineId).orElseThrow(() -> new DomainRuleException("Airline not found to create the leg"));
         Aircraft aircraft = airline.getAircraft(aircraftId).orElseThrow(() -> new DomainRuleException("Aircraft not found to create the leg"));
+        legOverlapValidator.validateNoOverlap(aircraftId, departureTime, arrivalTime);
         Leg leg = Leg.createLeg(id, originAirport, destinationAirport, departureTime, arrivalTime, aircraft, luggagePrice, price);
         airline.addLeg(leg.getId());
         legRepository.save(leg);
