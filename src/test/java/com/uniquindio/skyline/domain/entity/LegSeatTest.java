d8121749be3453747e9c225d21dd3e38f01d7e37
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-09-23 22:52:55 -0500

    fix: add missing id for LegSeat

--- src/test/java/com/uniquindio/skyline/domain/entity/LegSeatTest.java
+++ src/test/java/com/uniquindio/skyline/domain/entity/LegSeatTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.security.Security;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,14 +14,14 @@ public class LegSeatTest {
     @Test
     public void LegSeatIsCreatedAvailable() {
         // Checks the initial seat status.
-        LegSeat legSeat = LegSeat.createLegSeat("A32");
+        LegSeat legSeat = LegSeat.createLegSeat(UUID.randomUUID().toString(), "A32");
         assertEquals(SeatStatus.AVALIABLE, legSeat.getSeatStatus());
     }
 
     @Test
     public void LegSeatIsCreatedWthEmptyPassengerId() {
         // Checks that no passenger is assigned.
-        LegSeat legSeat = LegSeat.createLegSeat("B28");
+        LegSeat legSeat = LegSeat.createLegSeat(UUID.randomUUID().toString(), "B28");
         assertEquals(Optional.empty(), legSeat.getPassengerId());
     }
 
